182d009fd02e972b8096ce86f24ca77d8f3834ac
pestrukha <sturbauen@gmail.com>
2024-09-02 18:00:02 +0400

    17 Обновление

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView, ArticleView, ArticleFormCreateView
+from .views import IndexView, ArticleView, ArticleFormCreateView, ArticleFormEditView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='articles'),
     path('<int:id>/', ArticleView.as_view(), name='article_detail'),
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
+    path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -6,6 +6,25 @@ from .models import Article
 from .forms import ArticleForm
 
 
+class ArticleFormEditView(View):
+
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(request, 'articles/update.html', {'form': form, 'article_id': article_id})
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles')
+
+        return render(request, 'articles/update.html', {'form': form, 'article_id': article_id})
+
+
 class ArticleFormCreateView(View):
 
     def get(self, request, *args, **kwargs):
